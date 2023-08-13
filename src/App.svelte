e4a7498af3a2fb462d798cecccdeb26ac420d65b
XYZ <xyzscer@outlook.com>
2023-08-13 20:24:22 +0800

    day 1: 基本形态

---
+++ src/App.svelte
@@ -0,0 +1,29 @@
+<script>
+  import Editor from "./lib/Editor.svelte";
+  import Home from "./lib/Home.svelte";
+
+  import "./styles.css";
+
+  let code = "";
+  let startEdit = false;
+</script>
+
+<main class="container">
+  <nav style="grid-column: 1 / 24;grid-row: 1 / 2;background:lightblue">
+    nav
+  </nav>
+  <div
+    class="toolbar"
+    style=" grid-column: 1 / 2;grid-row: 1 / 24;background:lightgreen"
+  >
+    Toolbar
+  </div>
+  {#if startEdit}
+    <Editor bind:value={code} on:input={() => {}} />
+  {:else}
+    <Home />
+  {/if}
+</main>
+
+<style>
+</style>
---
+++ src/lib/Editor.svelte
@@ -0,0 +1,20 @@
+<script>
+</script>
+
+
+<textarea class="Editor" />
+
+
+<style>
+  .Editor {
+    display: block;
+    grid-column: 2/24;
+    grid-row: 2/24;
+
+    font-family: "Maple Mono","Fira Code", consolas, "Courier New", Courier, monospace;
+    font-size: 1rem;
+
+    outline: none;
+    border: none;
+  }
+</style>
